d33dc2ecb71bf5665113d25af3a61d1555b12646
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-09-24 13:27:17 -0700

    Main project setup

---
+++ CardioHealth/CardioHealth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardioHealth
+//
+//  Created by Dhanush Balachandran on 9/24/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
