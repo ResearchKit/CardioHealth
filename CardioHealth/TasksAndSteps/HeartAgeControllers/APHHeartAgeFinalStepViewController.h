6379705429b38025b1c2ffd3d383b5f605553624
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2014-09-29 13:46:39 -0700

    Updating to use the AppleCore circular progress.

--- CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeFinalStepViewController.h
+++ CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeFinalStepViewController.h
@@ -7,12 +7,11 @@
 //
 
 #import <APCAppleCore/APCAppleCore.h>
-#import "APHCircularProgressView.h"
 #import "APHHeartAgeVersusView.h"
 
 @interface APHHeartAgeFinalStepViewController : APCStepViewController
 
-@property (weak, nonatomic) IBOutlet APHCircularProgressView *circularProgress;
+@property (weak, nonatomic) IBOutlet APCCircularProgressView *circularProgress;
 @property (weak, nonatomic) IBOutlet APHHeartAgeVersusView *ageVersusHeartAge;
 
 @end
