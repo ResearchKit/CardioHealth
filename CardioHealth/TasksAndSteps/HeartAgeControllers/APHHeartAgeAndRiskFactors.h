064ca0d5d02b249efc0a3d0c9323ffb6e3b20406
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2014-10-06 09:58:32 -0700

    Adding data point keys that will be used as the keys for the results dictionary.

--- CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeAndRiskFactors.h
+++ CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeAndRiskFactors.h
@@ -9,6 +9,19 @@
 #import <Foundation/Foundation.h>
 #import <math.h>
 
+static NSString *kHeartAgeTestDataAge = @"heart-age-data-age";
+static NSString *kHeartAgekHeartAgeTestDataTotalCholesterol = @"heart-age-data-total-cholesterol";
+static NSString *kHeartAgekHeartAgeTestDataHDL = @"heart-age-data-hdl";
+static NSString *kHeartAgekHeartAgeTestDataSystolicBP = @"heart-age-data-systolicBP";
+static NSString *kHeartAgekHeartAgeTestDataSmoke = @"heart-age-data-smoke";
+static NSString *kHeartAgekHeartAgeTestDataDiabetes = @"heart-age-data-diabetes";
+static NSString *kHeartAgekHeartAgeTestDataFamilyDiabetes = @"heart-age-data-family-diabetes";
+static NSString *kHeartAgekHeartAgeTestDataFamilyHeart = @"heart-age-data-family-heart";
+static NSString *kHeartAgekHeartAgeTestDataEthnicity = @"heart-age-data-ethnicity";
+static NSString *kHeartAgekHeartAgeTestDataGender = @"heart-age-data-gender";
+static NSString *kHeartAgekHeartAgeTestDataCurrentlySmoke = @"heart-age-data-currently-smoke";
+static NSString *kHeartAgekHeartAgeTestDataHypertension = @"heart-age-data-hypertension";
+
 @interface APHHeartAgeAndRiskFactors : NSObject
 
 - (NSDictionary *)calculateHeartAgeAndTenYearRisk:(NSDictionary *)results;
