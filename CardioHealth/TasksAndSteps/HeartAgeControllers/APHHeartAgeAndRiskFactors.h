8ab4292e3f8fe60bbd2e78d13e124d99b3299b46
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2014-10-06 09:06:14 -0700

    Separating the heart age and risk calculations from the view controller.

---
+++ CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeAndRiskFactors.h
@@ -0,0 +1,13 @@
+//
+//  HeartAgeAndRiskFactors.h
+//  CardioHealth
+//
+//  Created by Farhan Ahmed on 10/6/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HeartAgeAndRiskFactors : NSObject
+
+@end
---
+++ CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeAndRiskFactors.m
@@ -0,0 +1,13 @@
+//
+//  HeartAgeAndRiskFactors.m
+//  CardioHealth
+//
+//  Created by Farhan Ahmed on 10/6/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import "HeartAgeAndRiskFactors.h"
+
+@implementation HeartAgeAndRiskFactors
+
+@end
