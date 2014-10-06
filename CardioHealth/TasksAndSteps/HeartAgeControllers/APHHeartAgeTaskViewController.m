9ac7ec98903665728ec5f24d7439a25b05cfaa1d
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2014-10-06 10:00:32 -0700

    Using the keys from the APHHeartAgeAndriskFactors class for the step identifiers.

--- CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeTaskViewController.m
+++ CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeTaskViewController.m
@@ -9,20 +9,8 @@
 #import "APHHeartAgeSummaryViewController.h"
 #import "APHHeartAgeAndRiskFactors.h"
 
-// Question Keys
+// Introduction Step Key
 static NSString *kHeartAgeIntroduction = @"HeartAgeIntroduction";
-static NSString *kHeartAgeQuestionAge = @"HeartAgeQuestion1";
-static NSString *kHeartAgeQuestionTotalCholesterol = @"HeartAgeQuestion2";
-static NSString *kHeartAgeQuestionHDL = @"HeartAgeQuestion3";
-static NSString *kHeartAgeQuestionSystolicBP = @"HeartAgeQuestion4";
-static NSString *kHeartAgeQuestionSmoke = @"HeartAgeQuestion5";
-static NSString *kHeartAgeQuestionDiabetes = @"HeartAgeQuestion6";
-static NSString *kHeartAgeQuestionFamilyDiabetes = @"HeartAgeQuestion7";
-static NSString *kHeartAgeQuestionFamilyHeart = @"HeartAgeQuestion8";
-static NSString *kHeartAgeQuestionEthnicity = @"HeartAgeQuestion9";
-static NSString *kHeartAgeQuestionGender = @"HeartAgeQuestion10";
-static NSString *kHeartAgeQuestionSmokeB = @"HeartAgeQuestion11";
-static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
 
 @interface APHHeartAgeTaskViewController ()
 
@@ -56,7 +44,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         RKNumericAnswerFormat *format = [RKNumericAnswerFormat integerAnswerWithUnit:nil];
         format.minimum = @(1);
         
-        RKQuestionStep* step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionAge
+        RKQuestionStep* step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeTestDataAge
                                                                      name:@"YourAge"
                                                                  question:NSLocalizedString(@"What is your age?", @"What is your age?")
                                                                    answer:format];
@@ -69,7 +57,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         RKAnswerFormat *format = [RKChoiceAnswerFormat choiceAnswerWithOptions:@[@"Female", @"Male"]
                                                                          style:RKChoiceAnswerStyleSingleChoice];
         
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionGender
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataGender
                                                                      name:@"Gender"
                                                                  question:NSLocalizedString(@"What is your biological sex?", @"What is your biological sex?")
                                                                    answer:format];
@@ -82,7 +70,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         RKAnswerFormat *format = [RKChoiceAnswerFormat choiceAnswerWithOptions:@[@"African-American", @"Other"]
                                                                          style:RKChoiceAnswerStyleSingleChoice];
 
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionEthnicity
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataEthnicity
                                                                      name:@"Ethnicity"
                                                                  question:@"What is your ethnic group?"
                                                                    answer:format];
@@ -95,7 +83,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         RKNumericAnswerFormat *format = [RKNumericAnswerFormat integerAnswerWithUnit:nil];
         format.minimum = @(0);
 
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionTotalCholesterol
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataTotalCholesterol
                                                                      name:@"TotalCholesterol"
                                                                  question:NSLocalizedString(@"What is your Total Cholesterol?", @"What is your Total Cholesterol?")
                                                                    answer:format];
@@ -108,7 +96,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         RKNumericAnswerFormat *format = [RKNumericAnswerFormat integerAnswerWithUnit:nil];
         format.minimum = @(0);
 
-        RKQuestionStep* step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionHDL
+        RKQuestionStep* step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataHDL
                                                                      name:@"HDLCholesterol"
                                                                  question:NSLocalizedString(@"What is your HDL Cholesterol?", @"What is your HDL Cholesterol?")
                                                                    answer:format];
@@ -121,7 +109,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         RKNumericAnswerFormat *format = [RKNumericAnswerFormat integerAnswerWithUnit:nil];
         format.minimum = @(0);
         
-        RKQuestionStep* step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionSystolicBP
+        RKQuestionStep* step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataSystolicBP
                                                                      name:@"SystolicBP"
                                                                  question:NSLocalizedString(@"What is your Systolic Blood Pressure?", @"What is your Systolic Blood Pressure?")
                                                                    answer:format];
@@ -131,7 +119,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
     }
     
     {
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionSmoke
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataSmoke
                                                                      name:@"SmokeA"
                                                                  question:NSLocalizedString(@"Have you ever smoked?", @"Have you ever smoked?")
                                                                    answer:[RKBooleanAnswerFormat new]];
@@ -141,7 +129,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
     }
     
     {
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionSmokeB
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataCurrentlySmoke
                                                                      name:@"SmokeB"
                                                                  question:NSLocalizedString(@"Do you still smoke?", @"Do you still smoke?")
                                                                    answer:[RKBooleanAnswerFormat new]];
@@ -151,8 +139,8 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
     }
     
     {
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionDiabetes
-                                                                     name:@"MedicalConditions"
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataDiabetes
+                                                                     name:@"Diabetes"
                                                                  question:NSLocalizedString(@"Do you have Diabetes?", @"Do you have Diabetes?")
                                                                    answer:[RKBooleanAnswerFormat new]];
         step.optional = NO;
@@ -161,7 +149,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
     }
     
     {
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionHypertension
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataHypertension
                                                                      name:@"Hypertension"
                                                                  question:NSLocalizedString(@"Are you being treated for Hypertension?",
                                                                                             @"Are you being treated for Hypertension?")
@@ -172,8 +160,8 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
     }
     
     {
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionFamilyDiabetes
-                                                                     name:@"FamilyHistory"
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataFamilyDiabetes
+                                                                     name:@"FamilyDiabetes"
                                                                  question:NSLocalizedString(@"Does Diabetes run in your family?", @"Does Diabetes run in your family?")
                                                                    answer:[RKBooleanAnswerFormat new]];
         step.optional = NO;
@@ -182,7 +170,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
     }
     
     {
-        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgeQuestionFamilyHeart
+        RKQuestionStep *step = [RKQuestionStep questionStepWithIdentifier:kHeartAgekHeartAgeTestDataFamilyHeart
                                                                      name:@"ParentHistory"
                                                                  question:NSLocalizedString(@"Have either of your parents had heart problems?", @"Have either of your parents had heart problems?")
                                                                    answer:[RKBooleanAnswerFormat new]];
@@ -221,7 +209,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
     
     APCAppDelegate *apcAppDelegate = [[UIApplication sharedApplication] delegate];
     
-    if ([viewController.step.identifier isEqualToString:kHeartAgeQuestionAge]) {
+    if ([viewController.step.identifier isEqualToString:kHeartAgeTestDataAge]) {
         // Check if we have the date of birth available via HealthKit
         if (apcAppDelegate.dataSubstrate.currentUser.consented) {
             NSDate *dateOfBirth = apcAppDelegate.dataSubstrate.currentUser.birthDate;
@@ -258,7 +246,7 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         // Normalize survey results into dictionary.
         for (RKQuestionResult *questionResult in surveyResult.surveyResults) {
             NSString *questionIdentifier = [[questionResult itemIdentifier] stringValue];
-            if ([questionIdentifier isEqualToString:kHeartAgeQuestionEthnicity] || [questionIdentifier isEqualToString:kHeartAgeQuestionGender]) {
+            if ([questionIdentifier isEqualToString:kHeartAgekHeartAgeTestDataEthnicity] || [questionIdentifier isEqualToString:kHeartAgekHeartAgeTestDataGender]) {
                 [surveyResultsDictionary setObject:(NSString *)questionResult.answer forKey:questionIdentifier];
             } else {
                 [surveyResultsDictionary setObject:(NSNumber *)questionResult.answer forKeyedSubscript:questionIdentifier];
@@ -267,13 +255,13 @@ static NSString *kHeartAgeQuestionHypertension = @"HeartAgeQuestion12";
         
         // Kickoff heart age calculations
         APHHeartAgeAndRiskFactors *heartAgeAndRiskFactors = [[APHHeartAgeAndRiskFactors alloc] init];
-        NSDictionary *heartAgeInfo = [heartAgeAndRiskFactors calculateHeartAgeAndTenYearRisk:surveyResultsDictionary];
+        NSDictionary *heartAgeInfo = [heartAgeAndRiskFactors calculateHeartAgeAndRiskFactors:surveyResultsDictionary];
         
         UIStoryboard *sbHeartAgeSummary = [UIStoryboard storyboardWithName:@"HeartAgeSummary" bundle:nil];
         APHHeartAgeSummaryViewController *heartAgeResultsVC = [sbHeartAgeSummary instantiateInitialViewController];
 
         heartAgeResultsVC.taskProgress = 0.25;
-        heartAgeResultsVC.actualAge = [surveyResultsDictionary[kHeartAgeQuestionAge] integerValue];
+        heartAgeResultsVC.actualAge = [surveyResultsDictionary[kHeartAgeTestDataAge] integerValue];
         heartAgeResultsVC.heartAge = [heartAgeInfo[@"age"] integerValue];
         heartAgeResultsVC.tenYearRisk = heartAgeInfo[@"tenYearRisk"];
         heartAgeResultsVC.lifetimeRisk = heartAgeInfo[@"lifetimeRisk"];
