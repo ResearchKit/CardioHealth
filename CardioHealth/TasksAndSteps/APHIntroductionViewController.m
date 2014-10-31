ef3fe5da38f5cffc64238719bc21be0f2bfa5071
Justin Warmkessel <justin.w@ymedialabs.com>
2014-10-30 17:22:48 -0700

    Added button and delegate to accessory view belonging to the introduction view controller. Modally present more important details.

--- CardioHealth/TasksAndSteps/APHIntroductionViewController.m
+++ CardioHealth/TasksAndSteps/APHIntroductionViewController.m
@@ -25,13 +25,33 @@ static  NSString  *kSuperClassName = @"APCIntroductionViewController";
 - (void)setupWithInstructionalImages:(NSArray *)imageNames andParagraphs:(NSArray *)paragraphs
 {
     [super setupWithInstructionalImages:imageNames andParagraphs:paragraphs];
+    
+    UIView *accessoryView = [super accessoryView];
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    CGRect rect = CGRectMake(0.0, 0.0, CGRectGetWidth(accessoryView.frame), CGRectGetHeight(accessoryView.frame));
+    button.frame = rect;
+    [accessoryView addSubview:button];
+    
+    button.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [button setTitle:@"View Important Details" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor appPrimaryColor] forState:UIControlStateNormal];
+    
+    [button addTarget:self action:@selector(viewImportantDetailButton) forControlEvents:UIControlEventTouchUpInside];
 }
 
+- (void)viewImportantDetailButton {
+    if ([self.delegate respondsToSelector:@selector(viewImportantDetailsSelected:)] ) {
+        [self.delegate viewImportantDetailsSelected:self];
+    }
+}
 #pragma  mark  -  View Controller Methods
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:kSuperClassName bundle:[NSBundle appleCoreBundle]];
+    
+    
     if (self != nil) {
         
     }
