57b840d3bfde6e07ef56e748e61386c27e90f409
Justin Warmkessel <justin.w@ymedialabs.com>
2014-12-02 16:37:35 -0800

    Inheriting from different class.
    Added delegate method to manage adding title to the first step for this 7 day fittness task.

--- CardioHealth/TasksAndSteps/Seven Day Allocation/APHSevenDayFitnessAllocationTaskViewController.h
+++ CardioHealth/TasksAndSteps/Seven Day Allocation/APHSevenDayFitnessAllocationTaskViewController.h
@@ -8,6 +8,6 @@
 
 @import APCAppleCore;
 
-@interface APHSevenDayFitnessAllocationTaskViewController : APCBaseTaskViewController
+@interface APHSevenDayFitnessAllocationTaskViewController : APCBaseWithProgressTaskViewController
 
 @end
