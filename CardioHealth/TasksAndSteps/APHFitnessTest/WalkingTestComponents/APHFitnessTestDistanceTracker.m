a287065bdc2e7baf7333496f5bcf42f5ac885634
Edward Cessna <edward.cessna@ymedialabs.com>
2014-12-06 09:53:20 -0800

    [APPLE-698]Set copyright attribution to Apple
    [APPLE-698]After discussion with Apple Legal, the copyright attribution for the app should be "Apple, Inc.".

--- CardioHealth/TasksAndSteps/APHFitnessTest/WalkingTestComponents/APHFitnessTestDistanceTracker.m
+++ CardioHealth/TasksAndSteps/APHFitnessTest/WalkingTestComponents/APHFitnessTestDistanceTracker.m
@@ -2,7 +2,7 @@
 //  APHFitnessTestDistanceTracker.m 
 //  MyHeartCounts 
 // 
-//  Copyright (c) 2014 <INSTITUTION-NAME-TBD> All rights reserved. 
+//  Copyright (c) 2014 Apple, Inc. All rights reserved. 
 // 
  
 #import "APHFitnessTestDistanceTracker.h"
--- CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeResultsViewController.h
+++ CardioHealth/TasksAndSteps/HeartAgeControllers/APHHeartAgeResultsViewController.h
@@ -2,7 +2,7 @@
 //  APHHeartAgeResultsViewController.h 
 //  MyHeartCounts 
 // 
-//  Copyright (c) 2014 <INSTITUTION-NAME-TBD> All rights reserved. 
+//  Copyright (c) 2014 Apple, Inc. All rights reserved. 
 // 
  
 #import <APCAppCore/APCAppCore.h>
