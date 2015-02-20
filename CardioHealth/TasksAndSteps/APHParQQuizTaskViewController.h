d99196bd337a8cf9e8b82e6b1013249170b7f8f4
Justin Warmkessel <justin.w@ymedialabs.com>
2015-02-19 17:28:29 -0800

    [APPLE 2005] Implemented dynamic PAR-Q quiz
    [APPLE 2005] Implemented dynamic PAR-Q quiz

---
+++ CardioHealth/TasksAndSteps/APHParQQuizTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  APHParQQuizViewController.h
+//  MyHeartCounts
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import <ResearchKit/ResearchKit.h>
+@import  APCAppCore;
+
+@interface APHParQQuizTaskViewController : APCBaseTaskViewController
+
+@end
