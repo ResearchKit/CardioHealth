423a319dc409f908210d7616e33b00775836c1ad
Justin Warmkessel <justin.w@ymedialabs.com>
2015-02-22 11:49:44 -0800

    [APPLE-1685] Removing my name from header.

--- CardioHealth/TasksAndSteps/HeartAgeControllers/APHInstructionsForBelowTwentyTableViewCell.h
+++ CardioHealth/TasksAndSteps/HeartAgeControllers/APHInstructionsForBelowTwentyTableViewCell.h
@@ -1,8 +1,7 @@
 //
 //  APHInstructionsForBelowTwentyTableViewCell.h
-//  CardioHealth
+//  myHeartCounts
 //
-//  Created by Justin Warmkessel on 2/22/15.
 //  Copyright (c) 2015 Apple, Inc. All rights reserved.
 //
 
