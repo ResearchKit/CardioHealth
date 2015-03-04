b0333bf46085d9f64a71786ec9bcfcabfa51b7e8
Pablo Arce <pablo.arce@ymedialabs.com>
2015-03-03 21:25:00 -0800

    Updated content

---
+++ CardioHealth/TabBarControllers/Dashboard/APHDayOne.h
@@ -0,0 +1,13 @@
+//
+//  APHDayOne.h
+//  CardioHealth
+//
+//  Created by Pablo Arce on 3/3/15.
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import <APCAppCore/APCAppCore.h>
+
+@interface APHDayOne : APCGenericSurveyTaskViewController
+
+@end
