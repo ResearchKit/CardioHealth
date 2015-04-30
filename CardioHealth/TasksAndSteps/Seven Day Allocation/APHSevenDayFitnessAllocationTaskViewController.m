571acad796c3e58297a7e9ad529d264e8adaceb0
Justin Warmkessel <justin.w@ymedialabs.com>
2015-04-30 12:16:07 -0700

    [APPLE-2936] Wrote debug code that will eventually be moved and transformed into production code that transforms location data from RK 6MWT to displacement data.

--- CardioHealth/TasksAndSteps/Seven Day Allocation/APHSevenDayFitnessAllocationTaskViewController.m
+++ CardioHealth/TasksAndSteps/Seven Day Allocation/APHSevenDayFitnessAllocationTaskViewController.m
@@ -33,6 +33,8 @@
 
 #import "APHSevenDayFitnessAllocationTaskViewController.h"
 
+#warning TEMPORARY import
+#import <CoreLocation/CoreLocation.h>
 
 static NSString *kMainStudyIdentifier = @"com.cardioVascular.sevenDayFitnessAllocation";
 static NSString *kSevenDayFitnessInstructionStep = @"sevenDayFitnessInstructionStep";
@@ -134,5 +136,205 @@ static NSString *kSevenDayFitnessCompleteStep = @"sevenDayFitnessCompleteStep";
     [super taskViewController:taskViewController didFinishWithReason:reason error:error];
 }
 
+#warning TEMPORARY HELPER METHOD BEING TESTED HERE FOR EASE
+/*********************************************************************************/
+#pragma  mark  -  Helper methods
+/*********************************************************************************/
+- (NSString*)createResultSummary
+{
+    //At this point we have the file
+    
+    /* Get the contents of the location file
+     * Pass the contents into an object that will transform the data.
+     * Let THAT object have it's own data sink that will flush the content right away.
+     *
+     *
+     *
+    */
+    
+    NSArray* locationData =
+
+                @[
+                      @{
+                          @"altitude": @(-1.5396218299865723),
+                          @"coordinate": @{
+                              @"latitude": @(37.52551170711118),
+                              @"longitude": @(-122.26207610808791)
+                          },
+                          @"horizontalAccuracy": @(65),
+                          @"timestamp": @"2015-04-29T07:50:44-0700",
+                          @"verticalAccuracy": @(10)
+                      },
+                      @{
+                          @"altitude": @(-1.5396218299865723),
+                          @"coordinate": @{
+                              @"latitude": @(37.52550846481516),
+                              @"longitude": @(-122.26207870196802)
+                          },
+                          @"horizontalAccuracy": @(65),
+                          @"timestamp": @"2015-04-29T07:50:58-0700",
+                          @"verticalAccuracy": @(10)
+                      },
+                      @{
+                          @"altitude": @(-1.5396218299865723),
+                          @"coordinate": @{
+                              @"latitude": @(37.52550522475725),
+                              @"longitude":@( -122.2620812940452)
+                          },
+                          @"horizontalAccuracy": @(65),
+                          @"timestamp": @"2015-04-29T07:50:58-0700",
+                          @"verticalAccuracy": @(10)
+                      },
+                      @{
+                          @"altitude": @(-1.5804280042648318),
+                          @"coordinate": @{
+                              @"latitude": @(37.52548785521381),
+                              @"longitude": @(-122.26208838954248)
+                          },
+                          @"horizontalAccuracy": @(65),
+                          @"timestamp": @"2015-04-29T07:50:58-0700",
+                          @"verticalAccuracy": @(10)
+                      },
+                      @{
+                          @"altitude": @(-1.540157675743103),
+                          @"coordinate": @{
+                              @"latitude": @(37.525495069962595),
+                              @"longitude": @(-122.26210175043981)
+                          },
+                          @"horizontalAccuracy": @(65),
+                          @"timestamp": @"2015-04-29T07:51:00-0700",
+                          @"verticalAccuracy": @(10)
+                      }
+                      ];
+
+    
+    __weak typeof (self) weakSelf = self;
+    
+    void(^LocationDataTransformer)(NSArray*) = ^(NSArray* locations)
+    {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^
+        {
+            __strong typeof (self) strongSelf = weakSelf;
+            NSMutableArray* displacementData = [NSMutableArray new];
+            
+            if (locations)
+            {
+                CLLocation* previousCoor = nil;
+                
+                for (NSDictionary* location in locations)
+                {
+                    float lon = 0;
+                    
+                    if ([location objectForKey:@"coordinate"])
+                    {
+                        if ([[location objectForKey:@"coordinate"] objectForKey:@"longitude"])
+                        {
+                            lon = [[[location objectForKey:@"coordinate"] objectForKey:@"longitude"] floatValue];
+                        }
+                    }
+                    
+                    float lat = 0;
+                    
+                    if ([location objectForKey:@"coordinate"])
+                    {
+                        if ([[location objectForKey:@"coordinate"] objectForKey:@"latitude"])
+                        {
+                            lat = [[[location objectForKey:@"coordinate"] objectForKey:@"latitude"] floatValue];
+                        }
+                    }
+                    
+                    CLLocation* currentCoor = [[CLLocation alloc] initWithLatitude:lat longitude:lon];
+                    
+                    if(previousCoor)
+                    {
+                        id displacementDistance = [NSNull null];
+                        id direction            = [NSNull null];
+                        
+                        if ([currentCoor distanceFromLocation:previousCoor])
+                        {
+                            displacementDistance = @([currentCoor distanceFromLocation:previousCoor]);
+                        }
+                        
+                        NSDictionary* displacement = [strongSelf displacement:displacementDistance direction:direction fromLocationData:location];
+                        
+                        [displacementData addObject:displacement];
+                    }
+                    else
+                    {
+                        NSDictionary* displacement = [strongSelf displacement:[NSNull null] direction:[NSNull null]fromLocationData:location];
+                        
+                        [displacementData addObject:displacement];
+                    }
+                    
+                    previousCoor = currentCoor;
+                }
+                
+                if ([NSJSONSerialization isValidJSONObject:displacementData])
+                {
+                    NSDictionary *displacementDictionary = @{@"items" : displacementData};
+                    
+                    [APCDataArchiverAndUploader uploadDictionary:displacementDictionary withTaskIdentifier:@"6MWT Displacement Data" andTaskRunUuid:strongSelf.taskRunUUID];
+                }
+            }
+        });
+    };
+    
+    LocationDataTransformer(locationData);
+    
+    return nil;
+}
+
+- (NSDictionary*)displacement:(id)displacementDistance direction:(id)direction fromLocationData:(NSDictionary*)location
+{
+    id altitude             = [NSNull null];
+    id timestamp            = [NSNull null];
+    id horizontalAccuracy   = [NSNull null];
+    id verticalAccuracy     = [NSNull null];
+    
+    //  Expecting an NSNumber or null. But just in case we have this check here.
+    if (displacementDistance == nil)
+    {
+        displacementDistance = [NSNull null];
+    }
+    
+    if (direction == nil)
+    {
+        direction = [NSNull null];
+    }
+    
+    if ([location objectForKey:@"altitude"])
+    {
+        altitude = [location objectForKey:@"altitude"];
+    }
+    
+    if ([location objectForKey:@"timestamp"])
+    {
+        timestamp = [location objectForKey:@"timestamp"];
+    }
+    
+    if ([location objectForKey:@"horizontalAccuracy"])
+    {
+        horizontalAccuracy = [location objectForKey:@"horizontalAccuracy"];
+    }
+    
+    if ([location objectForKey:@"verticalAccuracy"])
+    {
+        verticalAccuracy = [location objectForKey:@"verticalAccuracy"];
+    }
+    
+    NSDictionary* displacement =
+    @{
+      @"altitude": altitude,
+      @"displacement": displacementDistance,
+      @"displacementUnit" : @"meters", //    always in meters
+      @"direction": direction,
+      @"direction": @"meters", //    always in meters
+      @"horizontalAccuracy": horizontalAccuracy,
+      @"timestamp": timestamp,
+      @"verticalAccuracy": verticalAccuracy
+      };
+    
+    return displacement;
+}
 
 @end
